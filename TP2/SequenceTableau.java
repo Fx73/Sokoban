82e9689e114339468393dc942ab2e2fcc5997d36
Fx <fx73000@yahoo.fr>
2020-01-21 00:04:14 +0100

    TP2 fini

--- TP2/SequenceTableau.java
+++ TP2/SequenceTableau.java
@@ -1,33 +1,41 @@
 package TP2;
 
+import java.util.Arrays;
+
 //Interface Sequence externe
 
 public class SequenceTableau implements Sequence{
-    private final static int MAXLEN = 1000;
+    private int MAXLEN = 1000;
 
     int tab[] = new int[MAXLEN];
+    int first = 0;
     int len = 0;
 
+    private void doubleTabSize(){
+        MAXLEN = MAXLEN * 2;
+        tab = Arrays.copyOf(tab, MAXLEN);
+    }
+    private int moinsUn(int n){if(n==0)return MAXLEN-1; else return n-1; }
+    private int plusUn(int n){if(n==MAXLEN-1)return 0; else return n+1; }
+    private int last(){if(first+len> MAXLEN-1)return first+len-MAXLEN; else return first+len; }
+
+
     public void insereTete(int element){
-        if(len==MAXLEN) throw new RuntimeException("Tableau Plein");
-        for (int i = len;i>0;i--){
-            tab[i]= tab[i-1];
-        }
-        tab[0]=element;
+        if(len==MAXLEN) doubleTabSize();
+        first = moinsUn(first);
+        tab[first]=element;
         len++;
     }
 
     public void insereQueue(int element){
-        if(len==MAXLEN) throw new RuntimeException("Tableau Plein");
-        tab[len]=element;
+        if(len==MAXLEN) doubleTabSize();
+        tab[last()]=element;
         len++;
     }
 
     public int extraitTete(){
-        int v = tab[0];
-        for (int i = 1;i<len;i++){
-            tab[i-1]= tab[i];
-        }
+        int v = tab[first];
+        first = plusUn(first);
         len--;
         return v;
     }
@@ -35,9 +43,5 @@ public class SequenceTableau implements Sequence{
     public boolean estVide(){
         return len == 0;
     }
-
-
-
-
-
+ 
 }
